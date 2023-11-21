6059109c2d744abac70ab70244a76f75c05d2c4f
Weneville <wenev.16@gmail.com>
2023-11-21 15:10:03 +0700

    feat: add validation and relation to Group

--- server/models/group.js
+++ server/models/group.js
@@ -10,17 +10,33 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Group.hasMany(models.UserGroup);
     }
   }
   Group.init({
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: "Group Name must not be empty"
+        },
+        notEmpty: {
+          args: true,
+          msg: "Group Name must not be empty"
+        },
+      }
     },
     groupPicture: {
       type: DataTypes.STRING,
-      defaultValue: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      defaultValue: 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      validate: {
+        isUrl: {
+          args: true,
+          msg: "Group Picture must be Url"
+        },
+      }
     }
   }, {
     sequelize,

7f31c943ef16e0bf2492a7fc512b118eb845059c
Mohamad Tantyo Wisnuwardhana <tantyowisnu@gmail.com>
2023-11-22 17:18:35 +0700

    Fix: login routes on client

--- client/foodie-hub/src/main.jsx
+++ client/foodie-hub/src/main.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   RouterProvider,
   createBrowserRouter,
   // redirect,
-} from 'react-router-dom';
-import './style.css';
+} from "react-router-dom";
+import "./style.css";
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
-// import LoginLayout from './layouts/LoginLayout';
-import MainLayout from './layouts/MainLayout';
-// import Login from './views/Login';
-import Group from './views/Group';
-import Home from './views/Home'
+import LoginLayout from './layouts/LoginLayout';
+import MainLayout from "./layouts/MainLayout";
+import Login from './views/Login';
+import Group from "./views/Group";
+import Home from "./views/Home";
 
 // Your web app's Firebase configuration``
 const firebaseConfig = {
@@ -25,8 +24,8 @@ const firebaseConfig = {
   projectId: "foodiehub-3ba28",
   storageBucket: "foodiehub-3ba28.appspot.com",
   messagingSenderId: "28675153325",
-  appId: "1:28675153325:web:fc79bcb6d296e90ed25524"
-}
+  appId: "1:28675153325:web:fc79bcb6d296e90ed25524",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -35,8 +34,8 @@ export const db = getFirestore(app);
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home/>
+    path: "/",
+    element: <Home />,
   },
   {
     path: "/",
@@ -44,28 +43,28 @@ const router = createBrowserRouter([
     children: [
       {
         path: "group/:groupId",
-        element: <Group />
+        element: <Group />,
       },
       {
-        path: "group/:groupId/edit"
-      }
-    ]
+        path: "group/:groupId/edit",
+      },
+    ],
   },
 
-  // {
-  //   path: "/login",
-  //   element: <LoginLayout />,
-  //   children: [
-  //     {
-  //       path: "",
-  //       element: <Login />
-  //     },
-  //   ]
-  // }
-])
+  {
+    path: "/login",
+    element: <LoginLayout />,
+    children: [
+      {
+        path: "",
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

e2410407a57ad1279f08266ea83c78607d68969e
preettypeanuts <mrafly212001@gmail.com>
2023-11-22 16:09:45 +0700

    create components login adn form

---
+++ template/components/Login.jsx
@@ -0,0 +1,20 @@
+export const Login = () => {
+  return (
+    <>
+      <section className="sectionLogin">
+        <div className="loginContainer">
+          <div className="welcomeText">
+            <h1>Welcome To FoodieHub</h1>
+            <p style={{ color: "rgba(0, 0, 0, 0.743)" }}>
+              "Welcome to FoodieHub, where the love for food unites. Embrace the
+              flavors within our group conversations, where every dish becomes a
+              topic of discussion. Let's explore the tastes and stories behind
+              each bite together with our community!"
+            </p>
+            <button className="btn btn-light">Login With Google</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};

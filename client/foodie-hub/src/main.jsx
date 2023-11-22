ad77f7cd477f720fc855e322d980f225786dc4ef
Weneville <wenev.16@gmail.com>
2023-11-22 12:33:04 +0700

    feat: setup firebase and router on client

--- client/foodie-hub/src/main.jsx
+++ client/foodie-hub/src/main.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {
+  RouterProvider,
+  createBrowserRouter,
+  redirect,
+} from 'react-router-dom';
+import './style.css';
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import LoginLayout from './layouts/LoginLayout';
+import MainLayout from './layouts/MainLayout';
+
+// Your web app's Firebase configuration``
+const firebaseConfig = {
+  apiKey: "AIzaSyAJqUtMzGsoOqOkA0zklllp4jamOLdilbc",
+  authDomain: "foodiehub-3ba28.firebaseapp.com",
+  projectId: "foodiehub-3ba28",
+  storageBucket: "foodiehub-3ba28.appspot.com",
+  messagingSenderId: "28675153325",
+  appId: "1:28675153325:web:fc79bcb6d296e90ed25524",
+  databaseURL: "https://foodiehub-3ba28-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "",
+      },
+      {
+        path: "group/:groupId"
+      },
+      {
+        path: ""
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <LoginLayout />,
+    children: [
+      {
+        path: ""
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

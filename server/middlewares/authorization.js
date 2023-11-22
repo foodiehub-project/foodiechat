0e8a36cb0309ac17b7dd8bced62638eea0caf749
Weneville <wenev.16@gmail.com>
2023-11-22 12:42:29 +0700

    feat: add authorization for user role

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -1,14 +1,25 @@
 'use strict';
+const { UserGroup } = require("../models");
+const group = require("../models/group");
 
 const authorizationAdminOnly = async (req, res, next) => {
     try {
-        if (req.user.role === "admin") {
+        const { groupId } = req.params;
+
+        const userGroup = await UserGroup.findOne({
+            where: {
+                UserId: req.user.id,
+                GroupId: groupId,
+            }
+        });
+
+        if (userGroup.role === "admin") {
             next()
         } else {
             throw { name: "Forbidden" }
         }
     } catch (error) {
-        next(error)
+        next(error);
     }
 }
 

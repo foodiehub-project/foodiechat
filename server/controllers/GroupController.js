2576d9bcac604ef0147019b45f8d3b831556fc02
Mohamad Tantyo Wisnuwardhana <tantyowisnu@gmail.com>
2023-11-23 08:40:10 +0700

    fix: post /groups

--- server/controllers/GroupController.js
+++ server/controllers/GroupController.js
@@ -1,14 +1,33 @@
 'use strict';
 const { User, Group, UserGroup } = require('../models');
 
+const cloudinary = require('cloudinary').v2;
+
+const { randomUUID } = require('crypto');
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+});
+
 class GroupController {
     static async addGroups(req, res, next) {
         try {
             const { name } = req.body;
             const UserId = req.user.id
 
+            if (!req.file) throw { name: 'ValidationError' };
+            const base64File = Buffer.from(req.file.buffer).toString('base64');
+            const dataURI = `data:${req.file.mimetype};base64,${base64File}`;
+            const data = await cloudinary.uploader.upload(dataURI, {
+                public_id: `${req.file.originalname}_${randomUUID()}`,
+                folder: 'foodie'
+            });
+
             const createGroup = await Group.create({
-                name: name,
+                groupPicture: data.secure_url,
+                name
             });
 
             const createAdmin = await UserGroup.create({

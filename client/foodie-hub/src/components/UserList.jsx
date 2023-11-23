e5a3b2d0599841eb28a04fff37e5095de390bbd7
Weneville <wenev.16@gmail.com>
2023-11-23 10:36:28 +0700

    feat: add User List in client for GroupChat

--- client/foodie-hub/src/components/UserList.jsx
+++ client/foodie-hub/src/components/UserList.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import swal from "sweetalert";
+import BASE_URL from "../BaseUrl";
 
 
 export default function UserList() {
@@ -9,14 +12,25 @@ export default function UserList() {
     useEffect(() => {
         let ignore = false;
 
-        fetchUsers(ignore)
-        console.log()
+        fetchUsers(ignore);
         return () => (ignore = true);
-    })
+    }, []);
 
     const fetchUsers = async (ignore) => {
         try {
+            const { data } = await axios({
+                method: "get",
+                url: `${BASE_URL}/groups/${groupId}`,
+                headers: {
+                    authorization: `Bearer ${localStorage.access_token}`
+                }
+            });
 
+            if(!ignore) {
+                setUsers(data.UserGroups);
+            }
+        }
+        catch(error) {
             swal({
                 text: error.response.data.message,
                 icon: "error"
@@ -26,13 +40,17 @@ export default function UserList() {
     return (
         <div className="userContext">
         <p className="userList">Chat Participant's</p>
-        <div className="containerUList">
-          <img
-            src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg"
-            className="rounded-circle user_img_msg"
-          />
-          <p className="userByList"></p>
-        </div>
+        {users.map((user, index) => {
+            return (
+                <div key={index} className="containerUList">
+                <img
+                    src={`${user.User.profilePicture}`}
+                    className="rounded-circle user_img_msg"
+                />
+                <p className="userByList">{user.User.fullName}</p>
+                </div>
+            )
+        })}
       </div>
     )
 }
